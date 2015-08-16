6cfaa9078dfad935c58f0d5ad5f0882f94eaf85c
Evan Lin <evanslin@gmail.com>
2015-08-16 19:12:20 +0800

    add for example code

---
+++ example_test.go
@@ -0,0 +1,35 @@
+package skiplist
+
+import (
+	"fmt"
+
+	skiplist "github.com/kkdai/skiplist"
+)
+
+func Example() {
+	//New a skiplist
+	sl := skiplist.NewSkipList()
+
+	//Insert search key 50, value "5", value could be anything.
+	sl.Insert(50, "5")
+	sl.Insert(40, "4")
+	sl.Insert(70, "7")
+	sl.Insert(100, "10")
+
+	//Search key, which time complexity O(log n)
+	ret, err := sl.Search(50)
+	if err == nil {
+		fmt.Println("key 50: val->", ret)
+	} else {
+		fmt.Println("Not found, ", err)
+	}
+
+	//Delete by search key
+	err = sl.Delete(70)
+	if err != nil {
+		fmt.Println("Delete not found")
+	}
+
+	//Display all skip list content.
+	sl.DisplayAll()
+}
